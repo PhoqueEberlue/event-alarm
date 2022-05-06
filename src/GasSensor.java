faad66e2d1c453fb20270c41dfbe2c550b252a7a
Maxime Paulin <paulinma@i5-87.univ-savoie.fr>
2022-05-06 15:03:13 +0200

    added sensors classes

---
+++ src/GasSensor.java
@@ -0,0 +1,43 @@
+public class GasSensor extends Sensors{
+
+    //----------CONSTRUCTORS----------
+    public GasSensor(String name, String room, int valMax, int valMin){
+        this.unit = "ppm";
+        this.name = name;
+        this.room = room;
+        this.valMax = valMax;
+        this.valMin = valMin;
+    }
+
+    public GasSensor(String name, String room){
+        this.unit = unit;
+        this.name = name;
+        this.room = room;
+        this.valMax = 1000;
+        this.valMin = 0;
+    }
+
+    //----------TO STRING----------
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "GasSensor{}";
+    }
+    //----------GETTER----------
+    public String getRoom(){
+        return this.room;
+    }
+
+    public String getUnit(){
+        return this.unit;
+    }
+
+    public String getValMax(){
+        return this.valMax;
+    }
+    public String getValMin(){
+        return this.;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ src/Sensors.java
@@ -0,0 +1,7 @@
+public interface Sensors{
+    private String name;
+    private String room;
+    private int valMax;
+    private int valMin;
+    private String unit;
+}
